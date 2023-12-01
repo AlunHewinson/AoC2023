869bf5bf9369625a14f5b7045d88f1a0384707df
Alun Hewinson <alunhewinson@gmail.com>
2023-12-01 18:14:09 +0000

    code was wrong but I got lucky with it giving the right answer. here's a bodgy solution til I have time to refactor it sensibly

--- src/main/scala/aoc2023/day01.scala
+++ src/main/scala/aoc2023/day01.scala
@@ -2,6 +2,9 @@ package aoc2023
 
 import utils.utils.readDay
 
+import scala.collection.immutable.Seq
+import scala.util.matching.Regex
+
 object day01 extends App {
   // https://adventofcode.com/2023/day/1
 
@@ -10,18 +13,59 @@ object day01 extends App {
     val inp: String = readDay(day, test, year = 2023)
     val instructionSeparator: String = "\r*\n"
     val instructions: Array[String] = instructionSeparator.r.split(inp)
+
+    instructions.map(extractFirstAndLastDigits).foreach(println)
     instructions.map(extractFirstAndLastDigits).sum
 
   }
 
   private def extractFirstAndLastDigits(inputString: String): Int = {
-    val elfDigits: String = "0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine"
-    val foundElfMatches: Seq[String] = elfDigits.r.findAllIn(inputString).toList //findAllMatchIn(inputString).toList
+    val elfNumbers: List[String] = List("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three",
+      "four", "five", "six", "seven", "eight", "nine")
+
+    //val elfMatches = elfNumbers.map(elfNumber => elfNumber.r.findAllMatchIn(inputString))
+    val elfMatchesNumbers1: Seq[Iterator[Regex.Match]] = elfNumbers.map(elfNumber => elfNumber.r.findAllMatchIn(inputString))
+    val elfMatchesNumbers2: Seq[Iterator[Regex.Match]] = elfNumbers.map(elfNumber => elfNumber.r.findAllMatchIn(inputString))
+    val elfMatchesStrings: Seq[Regex.MatchIterator] = elfNumbers.map(elfNumber => elfNumber.r.findAllIn(inputString))
+    val minStarts: Seq[Int] = elfMatchesNumbers1.map(matches => {
+      if (matches.isEmpty) {
+        1000000
+      } else {
+        matches.map(lign => {
+          lign.start
+        }).min
+      }
+    })
+//    println(minStarts)
+    val maxStarts: Seq[Int] = elfMatchesNumbers2.map(matches => {
+      if (matches.isEmpty) {
+        -1
+      } else {
+        matches.map(yo => {
+          yo.start
+        }).max
+      }
+    })
+//    println(maxStarts)
+    val matches = elfMatchesStrings.map(matches => {
+      if (matches.isEmpty) {
+        ""
+      } else {
+        matches.map(yo => {
+          yo
+        }).min
+      }
+    })
+
+    val firstNumberIndex = minStarts.zipWithIndex.minBy(_._1)._2
+    val lastNumberIndex = maxStarts.zipWithIndex.maxBy(_._1)._2
+
     val elfDigitsMap: Map[String, Int] = Map(
       "0" -> 0, "1" -> 1, "2" -> 2, "3" -> 3, "4" -> 4, "5" -> 5, "6" -> 6, "7" -> 7, "8" -> 8, "9" -> 9, "zero" -> 0,
       "one" -> 1, "two" -> 2, "three" -> 3, "four" -> 4, "five" -> 5, "six" -> 6, "seven" -> 7, "eight" -> 8, "nine" -> 9
     )
-    elfDigitsMap.getOrElse(foundElfMatches.head, 0) * 10 + elfDigitsMap.getOrElse(foundElfMatches.last, 0)
+    97
+    elfDigitsMap.getOrElse(matches(firstNumberIndex), 0) * 10 + elfDigitsMap.getOrElse(matches(lastNumberIndex), 0)
   }
 
   println(solveDay(1, true))
